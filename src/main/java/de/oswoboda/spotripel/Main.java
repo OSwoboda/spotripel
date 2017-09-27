0838dbac4e54e41aadb45f1a460a5aa2370a5f05
Oliver Swoboda <Oliver Swoboda>
2017-09-27 23:42:28 +0200

    added support for csubj

--- src/main/java/de/oswoboda/spotripel/Main.java
+++ src/main/java/de/oswoboda/spotripel/Main.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-	
+
 	private static String sent1 = "John was the CEO of a company.";
 	private static String sent2 = "Dole wasn't defeated by Clinton. Dole was defeated by John, who won the competition.";
 	private static String sent3 = "Last week Tom visited Sarah at home, but she wasn't ill.";
@@ -15,19 +15,19 @@ public class Main {
 	private static String sent7 = "Peter wasn't there, he was somewhere else.";
 	private static String sent8 = "Peter wasn't there, he was at home.";
 	private static String sent9 = "The door opened slowly.";
-	
-    public static void main(String[] args) throws FileNotFoundException {
-    	SPOBuilder builder = new SPOBuilder();
-    	File dir = new File("src/main/resources/AlbertJG_2011_bearbeitet/");
-    	for (File file : dir.listFiles()) {
-    		Scanner s = new Scanner(file);
-    		while(s.hasNextLine()) {
-    			builder.add(s.nextLine());
-    		}
-    	}
-    	System.out.println(builder.toString());
-        /*SPOBuilder builder = new SPOBuilder();
-        builder.add("Peter, Björn and John were singing in the garage.");
-        System.out.println(builder.toString());*/
-    }
+
+	public static void main(String[] args) throws FileNotFoundException {
+		SPOBuilder builder = new SPOBuilder();
+		File dir = new File("src/main/resources/AlbertJG_2011_bearbeitet/");
+		for (File file : dir.listFiles()) {
+			Scanner s = new Scanner(file);
+			while (s.hasNextLine()) {
+				builder.add(s.nextLine());
+			}
+		}
+		System.out.println(builder.toString());
+		// SPOBuilder builder = new SPOBuilder();
+		// builder.add(sent1);
+		// System.out.println(builder.toString());
+	}
 }
--- src/main/java/de/oswoboda/spotripel/SPOBuilder.java
+++ src/main/java/de/oswoboda/spotripel/SPOBuilder.java
@@ -2,6 +2,9 @@ package de.oswoboda.spotripel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.process.Morphology;
@@ -13,75 +16,86 @@ import edu.stanford.nlp.trees.GrammaticalRelation;
 import edu.stanford.nlp.trees.UniversalEnglishGrammaticalRelations;
 
 public class SPOBuilder {
-	
+
 	private List<Result> results = new ArrayList<>();
-	
+
 	private final GrammaticalRelation nsubj = UniversalEnglishGrammaticalRelations.NOMINAL_SUBJECT;
+	private final GrammaticalRelation csubj = UniversalEnglishGrammaticalRelations.CLAUSAL_SUBJECT;
 	private final GrammaticalRelation nsubjpass = UniversalEnglishGrammaticalRelations.NOMINAL_PASSIVE_SUBJECT;
+	private final GrammaticalRelation csubjpass = UniversalEnglishGrammaticalRelations.CLAUSAL_PASSIVE_SUBJECT;
 	private final GrammaticalRelation det = UniversalEnglishGrammaticalRelations.DETERMINER;
 	private final GrammaticalRelation cop = UniversalEnglishGrammaticalRelations.COPULA;
 	private final GrammaticalRelation dobj = UniversalEnglishGrammaticalRelations.DIRECT_OBJECT;
 	private final GrammaticalRelation neg = UniversalEnglishGrammaticalRelations.NEGATION_MODIFIER;
-	private final GrammaticalRelation nmod = UniversalEnglishGrammaticalRelations.NOMINAL_MODIFIER;
-	
+
 	private boolean stem = true;
 	private SemanticGraph sg = null;
-	
+
 	public SPOBuilder add(String text) {
 		Result result = new Result(new Sentence(text));
 		results.add(result);
 		sg = result.getSentence().dependencyGraph(SemanticGraphFactory.Mode.ENHANCED_PLUS_PLUS);
-		for (SemanticGraphEdge edge : sg.findAllRelns(nsubj)) {
+		for (SemanticGraphEdge edge : Stream.concat(sg.findAllRelns(nsubj).stream(), sg.findAllRelns(csubj).stream())
+				.collect(Collectors.toList())) {
 			IndexedWord target = edge.getTarget();
-			
+
 			result.addSubject(addDeterminer(target));
 			IndexedWord source = edge.getSource();
 			String sourceTag = source.tag();
-			
+
 			if (sourceTag.contains("NN") || sourceTag.contains("JJ") || sourceTag.contains("RB")) {
 				if (sourceTag.contains("NN")) {
 					// if the source is a noun (NN*), its the object
-					result.addObject(addDeterminer(source));				
+					result.addObject(addDeterminer(source));
 				} else {
-					// if the source is an adjective (JJ*) or an adverb (RB*), then there is no object
+					// if the source is an adjective (JJ*) or an adverb (RB*),
+					// then there is no object
 				}
 				if (sg.hasChildWithReln(source, cop)) {
-					result.addPredicate(negate(source)+stem(sg.getChildWithReln(source, cop)));
+					result.addPredicate(negate(source) + stem(sg.getChildWithReln(source, cop)));
 				}
 			} else {
-				// if the source is neither noun nor adjective nor adverb, its the predicate
-				result.addPredicate(negate(source)+stem(source));
-				
+				// if the source is neither noun nor adjective nor adverb, its
+				// the predicate
+				result.addPredicate(negate(source) + stem(source));
+
 				if (sg.hasChildWithReln(source, dobj)) {
 					result.addObject(addDeterminer(sg.getChildWithReln(source, dobj)));
 				}
 			}
 		}
-		for (SemanticGraphEdge edge : sg.findAllRelns(nsubjpass)) {
+		for (SemanticGraphEdge edge : Stream
+				.concat(sg.findAllRelns(nsubjpass).stream(), sg.findAllRelns(csubjpass).stream())
+				.collect(Collectors.toList())) {
 			IndexedWord target = edge.getTarget();
 			IndexedWord source = edge.getSource();
 			result.addObject(addDeterminer(target));
-			result.addPredicate(negate(source)+stem(source));
-			if (sg.hasChildWithReln(source, nmod)) {
-				result.addSubject(addDeterminer(sg.getChildWithReln(source, nmod)));
+			result.addPredicate(negate(source) + stem(source));
+			Set<IndexedWord> children = sg.getChildrenWithRelns(source,
+					UniversalEnglishGrammaticalRelations.getNmods());
+			if (!children.isEmpty()) {
+				if (children.size() == 1) {
+					result.addSubject(addDeterminer(children.iterator().next()));
+				}
 			}
 		}
-		
+
 		return this;
 	}
-	
+
 	private String negate(IndexedWord word) {
 		return (sg.hasChildWithReln(word, neg)) ? "¬" : "";
 	}
-	
+
 	private String stem(IndexedWord word) {
 		return (stem) ? Morphology.stemStatic(word.value(), word.tag()).value() : word.value();
 	}
-	
+
 	private String addDeterminer(IndexedWord word) {
-		return (sg.hasChildWithReln(word, det) ? sg.getChildWithReln(word, det).value()+" "+word.value() : word.value());
+		return (sg.hasChildWithReln(word, det) ? sg.getChildWithReln(word, det).value() + " " + word.value()
+				: word.value());
 	}
-	
+
 	public String toString() {
 		StringBuilder csv = new StringBuilder();
 		for (Result result : results) {
@@ -89,7 +103,7 @@ public class SPOBuilder {
 			csv.append(result.posList).append("\n");
 			csv.append(result);
 		}
-		
+
 		return csv.toString();
 	}
 
